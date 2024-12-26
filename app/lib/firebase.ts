578bbf03341228460fc5115b58386e410945ab93
muathzoubi <moathaza@gmail.com>
2024-12-26 13:39:15 -0800

    gfds

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1Tpv9S00TO__RCkAN95ydnMQDR7yEb0A",
-  authDomain: "csa3-e2b6a.firebaseapp.com",
-  projectId: "csa3-e2b6a",
-  storageBucket: "csa3-e2b6a.firebasestorage.app",
-  messagingSenderId: "328650323342",
-  appId: "1:328650323342:web:468ea6435238c0452be0df",
-  measurementId: "G-D32GDGT38Q"
-};
+  apiKey: "AIzaSyA1SDKIHFs8WCV6Km_ym6OIYImqEbfN7ZI",
+  authDomain: "amer-3f64c.firebaseapp.com",
+  databaseURL: "https://amer-3f64c-default-rtdb.firebaseio.com",
+  projectId: "amer-3f64c",
+  storageBucket: "amer-3f64c.firebasestorage.app",
+  messagingSenderId: "339491266126",
+  appId: "1:339491266126:web:10e27a59e59916f7dbed1f",
+  measurementId: "G-9R47D8Q7T1"
+}
 
 const app = initializeApp(firebaseConfig);
  const db = getFirestore(app);
