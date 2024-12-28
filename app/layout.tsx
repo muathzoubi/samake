91990bb043c986400634fdf0a15e6ae97189adc9
muathzoubi <moathaza@gmail.com>
2024-12-28 12:24:20 -0800

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,14 @@ import { CartProvider } from '@/components/cart-provider'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
+  description:'اكتشف منتجاتنا عالية الجودة من الأسماك الطازجة والمستوردة والروبيان المميز. اطلب الآن مع أفضل وأسرع خدمة توصيل اونلاين.  ',
   openGraph: {
     images: [
       {
         url: 'https://alwtaniah.com/nfc2.png',
         width: 1200,
         height: 630,
-        alt: 'My App Open Graph Image',
+        alt: 'الشركة الوطنية للأسماك',
       },
     ],
   },
