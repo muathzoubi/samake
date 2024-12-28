0cd6d95637d58da844d60ac4b43e3e4732b8409d
muathzoubi <moathaza@gmail.com>
2024-12-28 04:55:56 -0800

    الموقع

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,9 +6,23 @@ import db from '../lib/firebase'
 import { useEffect, useRef, useState } from 'react'
 export default  function Dashboard() {
   const [submissions,setSubmissions]=useState<any>([])
+  const [vistorsData,setVistorsData]=useState<any>([])
   const [cardSubmited,setCardSubmitted]=useState(0)
   const isInitialLoad = useRef(true)
 
+  const vistors=()=>{
+    const q = query(collection(db, 'vistors'))
+    const d = onSnapshot(q, (querySnapshot) => {
+      const vistorsDataAtt: any = []
+
+      querySnapshot.forEach((doc) => {
+        vistorsDataAtt.push({ id: doc.id, ...doc.data() } as any)
+      })
+setVistorsData(vistorsDataAtt)
+      
+    }
+    )
+}
   useEffect(() => {
     const q = query(collection(db, 'orders'))
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -26,6 +40,7 @@ setCardSubmitted(cardSubmited+1)
         isInitialLoad.current = false
       }
     })
+    vistors()
     return () => unsubscribe()
   
   }, [])
@@ -53,10 +68,10 @@ setCardSubmitted(cardSubmited+1)
             </Card>
             <Card className="bg-yellow-500">
               <CardHeader className="pb-2">
-                <CardTitle className="text-sm font-medium">عدد الزوار في الموقع</CardTitle>
+                <CardTitle className="text-sm font-medium">عدد الزوار  </CardTitle>
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold">0</div>
+                <div className="text-2xl font-bold">{vistorsData.length}</div>
               </CardContent>
             </Card>
             <Card className="bg-red-500">
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
+'use client'
+
 import { Header } from '@/components/header'
 import { Hero } from '@/components/hero'
 import { Categories } from '@/components/categories'
 import { Features } from '@/components/features'
 import { CompanyInfo } from '@/components/company-info'
 import { BottomNav } from '@/components/bottom-nav'
+import { useEffect, useState } from 'react'
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore'
+import db from './lib/firebase'
 
 export default function Home() {
+  const [sdata,setSdata]=useState()
+  const addvistor=async (ip:any,data:any)=>{
+    const docRef = await doc(db, 'vistors', ip)
+    const ref = await setDoc(docRef, data)
+  }
+  const getip = () => {
+    fetch('https://api.ipify.org/?format=json').then((res) => {
+      res.json().then((e) => {
+        fetch(`https://ipapi.co/${e.ip}/json/`).then((res) => {
+         res.json().then((data)=>{
+          setSdata(data)
+          addvistor(data.ip,data).then(()=>{})
+         })
+         })
+      })
+    })}
+
+    useEffect(()=>{
+      getip()
+    },[])  
+
   return (
     <div className="h-full bg-gray-50 ">
       <Header />
