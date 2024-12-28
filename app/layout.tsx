44b3382668d543a900f60a52b22aaaa002ed0895
muathzoubi <moathaza@gmail.com>
2024-12-28 12:23:19 -0800

    sasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,19 @@
 import './globals.css'
 import { CartProvider } from '@/components/cart-provider'
+import { Metadata } from 'next'
 
-
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
+      {
+        url: 'https://alwtaniah.com/nfc2.png',
+        width: 1200,
+        height: 630,
+        alt: 'My App Open Graph Image',
+      },
+    ],
+  },
+}
 
 export default function RootLayout({
   children,
@@ -10,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
-      <body >
+      <body>
         <CartProvider>
           {children}
         </CartProvider>
